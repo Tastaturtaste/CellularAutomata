5240983f8a4fc48f84daf83d11586fe11667765f
Tastaturtaste <joshua.moellers@gmx.net>
2019-05-17 18:19:34 +0200

    added timer as utility

--- GameOfLife/src/Application.cpp
+++ GameOfLife/src/Application.cpp
@@ -7,16 +7,8 @@
 
 int main()
 {
-	/*engine game_instance(std::make_unique<GoL>());
-	game_instance.Run();*/
-
-	using namespace std::chrono_literals;
-	int64_t dur;
-	{
-		Timer<std::chrono::microseconds> timer(&dur);
-		std::this_thread::sleep_for(1s);
-	}
-	std::cout << dur << " should be 1000000\n";
+	engine game_instance(std::make_unique<GoL>());
+	game_instance.Run();
 
 	return 0;
 }
